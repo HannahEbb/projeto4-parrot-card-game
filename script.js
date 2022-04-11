42265d2dd3e38780c09f409cc95a7df0ebac4608
Hannah Ebbinghaus <hannah.ebb.he@gmail.com>
2022-04-11 08:12:09 -0300

    array e innerHTML

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let numerocartas;
 let numeropares;
 let arraycartas = [];
 let arraytodascartas = [document.querySelector(".carta01"), document.querySelector(".carta02"), document.querySelector(".carta03"), document.querySelector(".carta04"), document.querySelector(".carta05"), document.querySelector(".carta06"), document.querySelector(".carta07"), document.querySelector(".carta08"), document.querySelector(".carta09"), document.querySelector(".carta10"), document.querySelector(".carta11"), document.querySelector(".carta12"), document.querySelector(".carta13"), document.querySelector(".carta14")];
-
+let cartas;
 
 quantasCartas();
 
@@ -29,12 +29,20 @@ arraycartas = arraycartas.sort(comparador);
 
 //EXIBIR O ARRAYCARTAS NA ORDEM EMBARALHADA DO ARRAY -- ver aulas de quarta, quinta e sexta
 function exibeCartas(arraycartas) {
+  cartas = document.querySelector(".cartas");
 
   for (let j = 0; j <= (arraycartas.length - 1); j++) {
-  arraycartas[j].classList.remove("escondido");
-  arraycartas[j].classList.add("carta-verso");
+   cartas.innerHTML += arraycartas[j]; 
+   arraycartas[j].classList.remove("escondido");
 
-  return arraytodascartas;
+  }
+
+
+  //for (let j = 0; j <= (arraycartas.length - 1); j++) {
+  //arraycartas[j].classList.remove("escondido");
+  //arraycartas[j].classList.add("carta-verso");
+
+  //return arraytodascartas;
 }
 
 }
